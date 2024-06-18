1529db63588b5f88ec6b02f0f8dbb79cf65403af
Valerian <pvalera@gmail.com>
2024-06-18 23:55:51 +0300

    bugfix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,45 +1,62 @@
 import pytest
-from httpx import AsyncClient
-from app.main import app
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.main import app  # импортируем FastAPI приложение
+from app.database import Base
+from app.routers.events import get_db
+from app.models import Sensor, Event
+from app.schemas import SensorCreate, EventCreate
 
+
+
+client = TestClient(app)
+
+
+# Тесты для сенсоров
 @pytest.mark.asyncio
 async def test_create_sensor():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/sensors/", json={"name": "Sensor1"})
-        assert response.status_code == 200
-        assert response.json()["name"] == "Sensor1"
+    response = client.post("/sensors/", json={"name": "Sensor1", "type": 0})
+    assert response.status_code == 200
+    assert response.json()["name"] == "Sensor1"
 
 @pytest.mark.asyncio
-async def test_get_sensors():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/sensors/")
-        assert response.status_code == 200
-        assert isinstance(response.json(), list)
+async def test_read_sensor():
+    response = client.get("/sensors/1")
+    assert response.status_code == 200
+    assert response.json()["name"] == "Sensor1"
 
+@pytest.mark.asyncio
+async def test_read_sensors():
+    response = client.get("/sensors/?skip=0&limit=10")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
+
+# Тесты для событий
 @pytest.mark.asyncio
 async def test_create_event():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/events/", json={"sensor_id": 1, "temperature": 25.5, "created_at": "2023-06-18T10:10:18.955535"})
-        assert response.status_code == 200
-        assert response.json()["temperature"] == 25.5
+    # Создадим датчик для привязки события
+    response = client.post("/events/", json={"sensor_id": 1, "temperature": 20.0, "name" : "Пошел дождь", "humidity": 100})
+    assert response.status_code == 200
+    assert response.json()["temperature"] == 20.0
 
 @pytest.mark.asyncio
-async def test_get_events():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/events/")
-        assert response.status_code == 200
-        assert isinstance(response.json(), list)
+async def test_read_event():
+    response = client.get("/events/1")
+    assert response.status_code == 200
+    assert response.json()["temperature"] == 20.0
 
 @pytest.mark.asyncio
-async def test_update_event():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.put("/events/1", json={"sensor_id": 1, "temperature": 30.0, "created_at": "2023-06-18T10:10:18.955535"})
-        assert response.status_code == 200
-        assert response.json()["temperature"] == 30.0
+async def test_read_events():
+    response = client.get("/events/?skip=0&limit=10")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
 
 @pytest.mark.asyncio
-async def test_delete_event():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.delete("/events/1")
-        assert response.status_code == 200
-        assert response.json()["message"] == "Event deleted"
+async def test_read_events_by_sensor():
+    response = client.get("/events/sensor/1")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
